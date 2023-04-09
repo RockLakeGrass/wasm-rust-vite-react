5451b0fb05d0e7bb599730d49c400cd363f3a8cf
RockLakeGrass <m17816713710@163.com>
2023-04-09 10:29:07 +0800

    first update

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { ViteRsw } from 'vite-plugin-rsw';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    ViteRsw(),
+    react()
+  ],
+})
